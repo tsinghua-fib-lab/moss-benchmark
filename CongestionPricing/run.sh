8102ff6afcb0f4a62b25506bfdd27e976edddc3a
yanjunbo <13008250033@163.com>
2025-01-07 14:44:58 +0800

    set device id = 1

--- CongestionPricing/run.sh
+++ CongestionPricing/run.sh
@@ -9,11 +9,12 @@ SMOOTH_CITIES="china_beijing_s china_shanghai_s france_paris_s us_newyork_s"
 # congested traffic condition
 CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
 
+# ATTENTION: 临时设置--device 1
 for city in $NORMAL_CITIES $SMOOTH_CITIES $CONGESTED_CITIES; do
     for algo in none random deltatoll; do
         # simulate from 7:00 to 10:00, i.e. start at 7*3600=25200 seconds and simulet 3*3600=10800 steps
-        python3 run_all.py --algo $algo --data data/$city --start 25200 --steps 10800 --exp $city
+        python3 run_all.py --algo $algo --data data/$city --start 25200 --steps 10800 --exp $city --device 1
     done
     # train for 4 hours
-    timeout 4h python3 run_all.py --algo eGCN --data data/$city --start 25200 --steps 10800 --exp $city
+    timeout 4h python3 run_all.py --algo eGCN --data data/$city --start 25200 --steps 10800 --exp $city --device 1
 done
