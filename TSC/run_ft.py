af119e816c00207d889849a55ffca619f2b59067
yanjunbo <13008250033@163.com>
2025-01-09 18:55:01 +0800

    add tqdm for TSC

--- TSC/run_ft.py
+++ TSC/run_ft.py
@@ -4,35 +4,44 @@ import time
 
 from engine import get_engine
 from moss import TlPolicy
+from tqdm import tqdm
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--exp", type=str, help="name of the experiment")
-    parser.add_argument('--data', type=str, default='./data/us_newyork')
-    parser.add_argument('--start', type=int, default=0)
-    parser.add_argument('--steps', type=int, default=3600)
-    parser.add_argument('--interval', type=int, default=30)
+    parser.add_argument("--data", type=str, default="./data/us_newyork")
+    parser.add_argument("--start", type=int, default=0)
+    parser.add_argument("--steps", type=int, default=3600)
+    parser.add_argument("--interval", type=int, default=30)
 
     args = parser.parse_args()
 
     if args.exp is None:
-        path = time.strftime('log/ft/%Y%m%d-%H%M%S')
+        path = time.strftime("log/ft/%Y%m%d-%H%M%S")
     else:
-        path = time.strftime(f'log/ft/{args.exp}/%Y%m%d-%H%M%S')
+        path = time.strftime(f"log/ft/{args.exp}/%Y%m%d-%H%M%S")
     if not os.path.exists(path):
         os.makedirs(path, exist_ok=True)
 
     eng = get_engine(
-        map_file=f'{args.data}/map.bin',
-        person_file=f'{args.data}/agents.bin',
+        map_file=f"{args.data}/map.bin",
+        person_file=f"{args.data}/agents.bin",
         start_step=args.start,
     )
     t = time.time()
     eng.set_tl_policy_batch([i for i in range(eng.junction_count)], TlPolicy.FIXED_TIME)
-    eng.next_step(args.steps)
-    with open(f'{path}/info.log', 'a') as f:
-        f.write(f"{eng.get_departed_person_average_traveling_time():.3f} {eng.get_finished_person_count()} {time.time()-t:.3f}\n")
+    total_step = args.steps
+    interval = args.interval
+    _to_run_step = [interval for _ in range(total_step // interval)]
+    if total_step % interval > 0:
+        _to_run_step.append(total_step % interval)
+    for step in tqdm(_to_run_step):
+        eng.next_step(step)
+    with open(f"{path}/info.log", "a") as f:
+        f.write(
+            f"{eng.get_departed_person_average_traveling_time():.3f} {eng.get_finished_person_count()} {time.time()-t:.3f}\n"
+        )
 
 
 main()
--- TSC/run_mp.py
+++ TSC/run_mp.py
@@ -4,6 +4,7 @@ import time
 
 from engine import get_engine
 from moss import TlPolicy
+from tqdm import tqdm
 
 
 def main():
@@ -30,7 +31,13 @@ def main():
     )
     t = time.time()
     eng.set_tl_policy_batch([i for i in range(eng.junction_count)], TlPolicy.MAX_PRESSURE)
-    eng.next_step(args.steps)
+    total_step = args.steps
+    interval = args.interval
+    _to_run_step = [interval for _ in range(total_step // interval)]
+    if total_step % interval > 0:
+        _to_run_step.append(total_step % interval)
+    for step in tqdm(_to_run_step):
+        eng.next_step(step)
     with open(f'{path}/info.log', 'a') as f:
         f.write(f"{eng.get_departed_person_average_traveling_time():.3f} {eng.get_finished_person_count()} {time.time()-t:.3f}\n")
 
