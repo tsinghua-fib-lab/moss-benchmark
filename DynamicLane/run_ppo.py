f8a0101c65f3c61a18fde264ff231447021f5e35
yanjunbo <13008250033@163.com>
2025-01-11 12:35:28 +0800

    comment out training_freq check in ppo

--- DynamicLane/run_ppo.py
+++ DynamicLane/run_ppo.py
@@ -686,7 +686,8 @@ def main():
                     f.write(msg)
             # if global_step >= next_save_step:
             #     torch.save(agent.state_dict(), f'{path}/ckpts/{global_step}.pt')
-            if global_step % args.training_freq == 0:
+            # if global_step % args.training_freq == 0:
+            if True:
                 currentATT = info["ATT-d"]  # type:ignore
                 if currentATT < bestATT:
                     bestATT = currentATT
