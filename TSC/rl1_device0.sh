20a0e1be0fcbd300e9bde58a8fd0cdc77eb96350
yanjunbo <13008250033@163.com>
2025-01-12 15:36:33 +0800

    update script

--- TSC/rl1_device0.sh
+++ TSC/rl1_device0.sh
@@ -9,17 +9,22 @@ set -e
 # # congested traffic condition
 # CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
 
+# china_beijing sim1跑完这几个算法
+# us_newyork单独拿出来给rl1跑efficient_mplight
 
-PART1_CITIES="china_beijing china_shanghai france_paris"
+PART0_CITIES="china_shanghai france_paris"
 
-PART2_CITIES="us_newyork china_beijing_s china_shanghai_s"
+# france_paris_s rl2跑完这几个算法
+PART1_CITIES="china_beijing_s china_shanghai_s us_newyork_s"
 
-PART3_CITIES="france_paris_s us_newyork_s china_beijing_c "
+# china_shanghai_c rl2跑完这几个算法
+PART2_CITIES="china_beijing_c france_paris_c us_newyork_c"
 
-PART4_CITIES="china_shanghai_c france_paris_c us_newyork_c"
+python3 run_efficient_mplight.py --data ./data/us_newyork --start 25200 --steps 10800 --exp us_newyork --device 0
 
-for city in $PART2_CITIES; do
-    for algo in ft mp ppo mplight efficient_mplight advanced_colight advanced_mplight colight frap; do
+for city in $PART0_CITIES; do
+    # for algo in ft mp ppo mplight efficient_mplight advanced_colight advanced_mplight colight frap; do
+    for algo in ft mp ppo mplight efficient_mplight; do
         # train for 4 hours
         # simulate from 7:00 to 10:00, i.e. start at 7*3600=25200 seconds and simulet 3*3600=10800 steps
         python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 0
--- TSC/rl2_device1.sh
+++ TSC/rl2_device1.sh
@@ -10,16 +10,20 @@ set -e
 # CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
 
 
-PART1_CITIES="china_beijing china_shanghai france_paris"
+# china_beijing sim1跑完这几个算法
+# us_newyork单独拿出来给rl1跑efficient_mplight
 
-PART2_CITIES="us_newyork china_beijing_s china_shanghai_s"
+PART0_CITIES="china_shanghai france_paris"
 
-PART3_CITIES="france_paris_s us_newyork_s china_beijing_c "
+# france_paris_s rl2跑完这几个算法
+PART1_CITIES="china_beijing_s china_shanghai_s us_newyork_s"
 
-PART4_CITIES="china_shanghai_c france_paris_c us_newyork_c"
+# china_shanghai_c rl2跑完这几个算法
+PART2_CITIES="china_beijing_c france_paris_c us_newyork_c"
 
-for city in $PART3_CITIES; do
-    for algo in ft mp ppo mplight efficient_mplight advanced_colight advanced_mplight colight frap; do
+for city in $PART1_CITIES; do
+    # for algo in ft mp ppo mplight efficient_mplight advanced_colight advanced_mplight colight frap; do
+    for algo in ft mp ppo mplight efficient_mplight; do
         # train for 4 hours
         # simulate from 7:00 to 10:00, i.e. start at 7*3600=25200 seconds and simulet 3*3600=10800 steps
         python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1
