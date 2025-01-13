9b1b4849d89b5875cb981e962aa5eb28f2624c75
yanjunbo <13008250033@163.com>
2025-01-13 14:15:47 +0800

    update scripts

--- TSC/rl2_ft_device1.sh
+++ TSC/rl2_ft_device1.sh
@@ -5,7 +5,7 @@ set -e
 
 PART0_CITIES="china_shanghai china_shanghai_s france_paris_s us_newyork_s"
 
-for city in $PART1_CITIES; do
+for city in $PART0_CITIES; do
     # for algo in ft mp ppo mplight efficient_mplight advanced_colight advanced_mplight colight frap; do
     for algo in ft mp ppo mplight; do
         # train for 4 hours
