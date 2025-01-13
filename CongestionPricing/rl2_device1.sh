14a9037c03b6d451f13e718db50948443f9a28b3
yanjunbo <13008250033@163.com>
2025-01-13 22:54:36 +0800

    update scripts

--- CongestionPricing/rl2_device1.sh
+++ CongestionPricing/rl2_device1.sh
@@ -7,7 +7,7 @@ PART0_CITIES="us_newyork france_paris_c us_newyork_c"
 for city in $PART0_CITIES; do
     for algo in none random deltatoll; do
         # simulate from 7:00 to 10:00, i.e. start at 7*3600=25200 seconds and simulet 3*3600=10800 steps
-        python3 run_all.py --algo $algo --data data/$city --start 25200 --steps 10800 --exp $city --device 3
+        python3 run_all.py --algo $algo --data data/$city --start 25200 --steps 10800 --exp $city --device 1
     done
-    python3 run_all.py --algo eGCN --data data/$city --start 25200 --steps 10800 --exp $city --device 3
+    python3 run_all.py --algo eGCN --data data/$city --start 25200 --steps 10800 --exp $city --device 1
 done
