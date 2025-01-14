ff146f232d5ceba43351a69f9023e562dcc61715
yanjunbo <13008250033@163.com>
2025-01-14 22:24:08 +0800

    update scripts

---
+++ DynamicLane/rl1_device1.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+set -x
+set -e
+
+PART0_CITIES="us_newyork france_paris_c us_newyork_c"
+
+echo "脚本已经开始将在7小时后继续执行。"
+
+sleep 7h
+
+echo "7小时已过脚本将继续执行。"
+
+for city in $PART0_CITIES; do
+    for algo in none random rule; do
+        # simulate from 7:00 to 10:00, i.e. start at 7*3600=25200 seconds and simulet 3*3600=10800 steps
+        python3 run_baseline.py --algo $algo --data data/$city --start 25200 --steps 10800 --exp $city --device 1
+    done
+    python3 run_ppo.py --data data/$city --start 25200 --steps 10800 --exp $city --device 1
+done
