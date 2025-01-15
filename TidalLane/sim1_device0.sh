05faf8654dae8443bb9f1cc31adfe36d701ca92b
yanjunbo <13008250033@163.com>
2025-01-15 16:53:40 +0800

    update scripts

--- TidalLane/sim1_device0.sh
+++ TidalLane/sim1_device0.sh
@@ -2,12 +2,26 @@
 set -x
 set -e
 
-PART0_CITIES="china_beijing china_beijing_s us_newyork_s china_shanghai_s china_shanghai_c us_newyork_c"
+# PART0_CITIES="china_beijing china_beijing_s us_newyork_s china_shanghai_s china_shanghai_c us_newyork_c"
+PART0_CITIES="china_shanghai_s china_shanghai_c us_newyork_c"
+
+echo "脚本已经开始将在7小时后继续执行。"
+
+sleep 7h
+
+echo "7小时已过脚本将继续执行。"
+
+python3 run_baseline.py --algo rule --data data/us_newyork_s --start 25200 --steps 10800 --exp us_newyork_s --device 0
+
+python3 run_ppo.py --data data/us_newyork_s --start 25200 --steps 10800 --exp us_newyork_s --device 0
 
 for city in $PART0_CITIES; do
-    for algo in none random rule; do
-        # simulate from 7:00 to 10:00, i.e. start at 7*3600=25200 seconds and simulet 3*3600=10800 steps
-        python3 run_baseline.py --algo $algo --data data/$city --start 25200 --steps 10800 --exp $city --device 0
-    done
+    
+    python3 run_baseline.py --algo none --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+    
+    python3 run_baseline.py --algo random --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+    
+    python3 run_baseline.py --algo rule --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+    
     python3 run_ppo.py --data data/$city --start 25200 --steps 10800 --exp $city --device 0
 done
