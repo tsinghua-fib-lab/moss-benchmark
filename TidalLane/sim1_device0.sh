09d8d04fd57c184d8a972e36f5ff627535032e80
yanjunbo <13008250033@163.com>
2025-01-15 10:30:36 +0800

    update scripts

---
+++ TidalLane/sim1_device0.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+set -x
+set -e
+
+PART0_CITIES="china_beijing china_beijing_s us_newyork_s china_shanghai_s china_shanghai_c us_newyork_c"
+
+for city in $PART0_CITIES; do
+    for algo in none random rule; do
+        # simulate from 7:00 to 10:00, i.e. start at 7*3600=25200 seconds and simulet 3*3600=10800 steps
+        python3 run_baseline.py --algo $algo --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+    done
+    python3 run_ppo.py --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+done
