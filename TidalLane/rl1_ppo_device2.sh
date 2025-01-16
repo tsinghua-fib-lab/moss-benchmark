9c1530ee8bfa073f17a88000c51df00f8222dddc
yanjunbo <13008250033@163.com>
2025-01-16 13:37:30 +0800

    update scripts

---
+++ TidalLane/rl1_ppo_device2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+set -x
+set -e
+# TODO:只差ppo的城市
+
+PART0_CITIES="us_newyork"
+
+for city in $PART0_CITIES; do
+    python3 run_ppo.py --data data/$city --start 25200 --steps 10800 --exp $city --device 2
+done
