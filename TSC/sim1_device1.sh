8b0a82bd232efb17060d62713c6a567bfa70d928
yanjunbo <13008250033@163.com>
2025-01-16 14:18:47 +0800

    update TSC scripts

--- TSC/sim1_device1.sh
+++ TSC/sim1_device1.sh
@@ -2,9 +2,6 @@
 set -x
 set -e
 
-# 这个是没跑完colight的城市
-UNDONE_NORMAL_CITIES="france_paris us_newyork"
-
 # 这些是还没跑完efficient_mplight的city
 # smooth traffic condition
 UNDONE_SMOOTH_CITIES="china_beijing_s china_shanghai_s us_newyork_s"
@@ -19,17 +16,6 @@ SMOOTH_CITIES="china_beijing_s china_shanghai_s france_paris_s us_newyork_s"
 CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
 
 
-for city in $UNDONE_NORMAL_CITIES; do
-    for algo in colight; do
-        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
-
-for city in $NORMAL_CITIES; do
-    for algo in advanced_mplight; do
-        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
 
 for city in $NORMAL_CITIES; do
     for algo in advanced_colight; do
