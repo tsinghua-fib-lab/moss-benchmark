673b279dc98a27ca2bf295f6f902e1612ca60332
yanjunbo <13008250033@163.com>
2025-01-16 14:20:51 +0800

    update TL scripts

--- TidalLane/sim1_ppo_device1.sh
+++ TidalLane/sim1_ppo_device1.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 # TODO:只差ppo的城市
 
-PART0_CITIES="us_newyork_s"
+PART0_CITIES="us_newyork us_newyork_s"
 
 for city in $PART0_CITIES; do
     python3 run_ppo.py --data data/$city --start 25200 --steps 10800 --exp $city --device 1
