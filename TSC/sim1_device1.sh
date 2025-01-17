46ec238b6c3a08cf7249edc0f790dde5378ba376
yanjunbo <13008250033@163.com>
2025-01-17 21:58:07 +0800

    更新sim1的使用脚本

--- TSC/sim1_device1.sh
+++ TSC/sim1_device1.sh
@@ -2,14 +2,6 @@
 set -x
 set -e
 
-# TODO:sim1二选一
-
-# 这些是还没跑完efficient_mplight的city
-# smooth traffic condition
-UNDONE_SMOOTH_CITIES="china_beijing_s china_shanghai_s us_newyork_s"
-# congested traffic condition
-UNDONE_CONGESTED_CITIES="china_beijing_c france_paris_c us_newyork_c"
-
 # normal traffic condition
 NORMAL_CITIES="china_beijing china_shanghai france_paris us_newyork"
 # smooth traffic condition
@@ -17,18 +9,42 @@ SMOOTH_CITIES="china_beijing_s china_shanghai_s france_paris_s us_newyork_s"
 # congested traffic condition
 CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
 
-
+# 正常城市的三个实验TSC部分
 
 for city in $NORMAL_CITIES; do
-    for algo in advanced_colight; do
+    for algo in mplight; do
+         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
+    done
+done
+
+for city in $NORMAL_CITIES; do
+    for algo in efficient_mplight; do
+         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
+    done
+done
+
+for city in $NORMAL_CITIES; do
+    for algo in advanced_mplight; do
         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
     done
 done
 
 # 其他拥堵条件
-for city in $UNDONE_SMOOTH_CITIES $UNDONE_CONGESTED_CITIES; do
+for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
+    for algo in mplight; do
+         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
+    done
+done
+
+for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
     for algo in efficient_mplight; do
-        python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1
+         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
+    done
+done
+
+for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
+    for algo in advanced_mplight; do
+        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
     done
 done
 
@@ -40,12 +56,6 @@ done
 
 for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
     for algo in colight; do
-        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
-
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in advanced_mplight; do
         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
     done
 done
