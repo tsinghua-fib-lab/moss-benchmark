275d1b65a320fe030265fea3901af049c3645de9
yanjunbo <13008250033@163.com>
2025-01-18 09:39:48 +0800

    update scripts

--- TSC/sim1_other_device1.sh
+++ TSC/sim1_other_device1.sh
@@ -2,32 +2,10 @@
 set -x
 set -e
 
-# normal traffic condition
-NORMAL_CITIES="china_beijing china_shanghai france_paris us_newyork"
 # smooth traffic condition
 SMOOTH_CITIES="china_beijing_s china_shanghai_s france_paris_s us_newyork_s"
 # congested traffic condition
 CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
-
-# 正常城市的三个实验TSC部分
-
-for city in $NORMAL_CITIES; do
-    for algo in mplight; do
-         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
-
-for city in $NORMAL_CITIES; do
-    for algo in efficient_mplight; do
-         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
-
-for city in $NORMAL_CITIES; do
-    for algo in advanced_mplight; do
-        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
 
 # 其他拥堵条件
 for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
