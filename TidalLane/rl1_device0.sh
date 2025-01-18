fcc43f6567c14c6fc0c759e0bbc9c9c73c467d68
yanjunbo <13008250033@163.com>
2025-01-18 21:21:02 +0800

    update scripts

--- TidalLane/rl1_device0.sh
+++ TidalLane/rl1_device0.sh
@@ -2,18 +2,18 @@
 set -x
 set -e
 
-PART0_CITIES="us_newyork_c"
+# PART0_CITIES="us_newyork_c"
 
-for city in $PART0_CITIES; do
+# for city in $PART0_CITIES; do
     
-    python3 run_baseline.py --algo none --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+#     python3 run_baseline.py --algo none --data data/$city --start 25200 --steps 10800 --exp $city --device 0
     
-    python3 run_baseline.py --algo random --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+#     python3 run_baseline.py --algo random --data data/$city --start 25200 --steps 10800 --exp $city --device 0
     
-    python3 run_baseline.py --algo rule --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+#     python3 run_baseline.py --algo rule --data data/$city --start 25200 --steps 10800 --exp $city --device 0
     
-    # python3 run_ppo.py --data data/$city --start 25200 --steps 10800 --exp $city --device 0
-done
+#     # python3 run_ppo.py --data data/$city --start 25200 --steps 10800 --exp $city --device 0
+# done
 
 PART1_CITIES="us_newyork_c"
 
