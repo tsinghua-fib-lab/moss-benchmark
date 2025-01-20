e1bd88976b59f951c3e774423679f2c39a0fea41
yanjunbo <13008250033@163.com>
2025-01-20 18:56:37 +0800

    update scripts

--- TSC/sim1_normal_device0.sh
+++ TSC/sim1_normal_device0.sh
@@ -3,23 +3,10 @@ set -x
 set -e
 
 # normal traffic condition
-NORMAL_CITIES="china_beijing china_shanghai france_paris us_newyork"
+UNDONE_NORMAL_CITIES="china_shanghai france_paris"
 
-# 正常城市的三个实验TSC部分
 
-for city in $NORMAL_CITIES; do
-    for algo in mplight; do
-         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 0 || true
-    done
-done
-
-for city in $NORMAL_CITIES; do
-    for algo in efficient_mplight; do
-         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 0 || true
-    done
-done
-
-for city in $NORMAL_CITIES; do
+for city in $UNDONE_NORMAL_CITIES; do
     for algo in advanced_mplight; do
         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 0 || true
     done
