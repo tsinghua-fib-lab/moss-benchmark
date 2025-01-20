c424d2c33951dbddfacf443a941a2b42c5f78273
yanjunbo <13008250033@163.com>
2025-01-20 19:04:09 +0800

    update terminated scripts

--- TSC/sim1_other_device1.sh
+++ TSC/sim1_other_device1.sh
@@ -7,12 +7,12 @@ SMOOTH_CITIES="china_beijing_s china_shanghai_s france_paris_s us_newyork_s"
 # congested traffic condition
 CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
 
-# 其他拥堵条件
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in mplight; do
-         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
+# # 其他拥堵条件
+# for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
+#     for algo in mplight; do
+#          timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
+#     done
+# done
 
 for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
     for algo in efficient_mplight; do
@@ -43,6 +43,3 @@ for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
     done
 done
-
-
- 
---
+++ TSC/sim1_terminated_device1.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+set -x
+set -e
+
+# terminated exps
+timeout 4h python3 run_efficient_mplight.py --data ./data/china_shanghai_s --start 25200 --steps 10800 --exp china_shanghai_s --device 1 || true
