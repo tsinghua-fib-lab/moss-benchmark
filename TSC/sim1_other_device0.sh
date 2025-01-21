433bc3a9d5d946bbbca666753322504e7ae09efe
yanjunbo <13008250033@163.com>
2025-01-21 10:04:49 +0800

    update scripts

--- TSC/sim1_other_device0.sh
+++ TSC/sim1_other_device0.sh
@@ -7,30 +7,6 @@ SMOOTH_CITIES="china_beijing_s china_shanghai_s france_paris_s us_newyork_s"
 # congested traffic condition
 CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
 
-# 其他拥堵条件
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in mplight; do
-         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 0 || true
-    done
-done
-
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in efficient_mplight; do
-         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 0 || true
-    done
-done
-
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in advanced_mplight; do
-        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 0 || true
-    done
-done
-
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in frap; do
-        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 0 || true
-    done
-done
 
 for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
     for algo in colight; do
--- TSC/sim1_other_device1.sh
+++ TSC/sim1_other_device1.sh
@@ -7,18 +7,7 @@ SMOOTH_CITIES="china_beijing_s china_shanghai_s france_paris_s us_newyork_s"
 # congested traffic condition
 CONGESTED_CITIES="china_beijing_c china_shanghai_c france_paris_c us_newyork_c"
 
-# # 其他拥堵条件
-# for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-#     for algo in mplight; do
-#          timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-#     done
-# done
-
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in efficient_mplight; do
-         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
+timeout 4h python3 run_efficient_mplight.py --data ./data/us_newyork_c --start 25200 --steps 10800 --exp us_newyork_c --device 1
 
 for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
     for algo in advanced_mplight; do
@@ -32,14 +21,15 @@ for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
     done
 done
 
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in colight; do
-        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
+# TODO:下面这俩给卡0跑了 所以记得杀
+# for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
+#     for algo in colight; do
+#         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
+#     done
+# done
 
-for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
-    for algo in advanced_colight; do
-        timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
-    done
-done
+# for city in $SMOOTH_CITIES $CONGESTED_CITIES; do
+#     for algo in advanced_colight; do
+#         timeout 4h python3 run_$algo.py --data ./data/$city --start 25200 --steps 10800 --exp $city --device 1 || true
+#     done
+# done
