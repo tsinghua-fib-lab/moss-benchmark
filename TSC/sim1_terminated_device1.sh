4239d2a1b19d987a40d30d574911fae210d8aa7f
yanjunbo <13008250033@163.com>
2025-01-23 21:59:29 +0800

    update scripts

--- TSC/sim1_terminated_device1.sh
+++ TSC/sim1_terminated_device1.sh
@@ -2,6 +2,6 @@
 set -x
 set -e
 
-sleep 11h
+sleep 10h
 # terminated exps
 timeout 4h python3 run_efficient_mplight.py --data ./data/china_shanghai_s --start 25200 --steps 10800 --exp china_shanghai_s --device 1 || true
